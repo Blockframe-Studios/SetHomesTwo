738a91a52d021fcb69ff32ce0d16b1d71c36707a
milanmalhotra <milmalho@gmail.com>
2026-08-21 23:12:56 -0400

    test: cover home creation and command teleport end to end

---
+++ src/test/e2e/homes.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect, waitUntil } from '@drownek/plugwright';
+
+test('a created home survives and is listed', async ({ player }) => {
+  await player.makeOp();
+
+  player.chat('/create-home base');
+  await expect(player).toHaveReceivedMessage('base has been created successfully');
+
+  player.chat('/list-homes');
+  await expect(player).toHaveReceivedMessage('base');
+});
+
+test('the go-home command teleports the player back', async ({ player }) => {
+  await player.makeOp();
+  await player.setGameMode('spectator');
+
+  const origin = player.bot.entity.position.clone();
+  player.chat('/create-home base');
+  await expect(player).toHaveReceivedMessage('base has been created successfully');
+
+  await player.teleport(origin.x + 60, origin.y, origin.z + 60);
+  await waitUntil(() => player.bot.entity.position.distanceTo(origin) > 20);
+
+  player.chat('/home base');
+  await expect(player).toHaveReceivedMessage('Teleported to base');
+  await waitUntil(() => player.bot.entity.position.distanceTo(origin) < 2);
+});
